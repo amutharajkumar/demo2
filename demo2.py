3badf09cfbc7f8a9b28fae585cbcd42487e06333
amutharajkumar <amutharajkumar95@gmail.com>
2024-07-05 09:45:55 -0700

    few changes done

--- demo2.py
+++ demo2.py
@@ -1,10 +1,9 @@
 A=[3,5,2,4,8,0,7]
-def search(n):
+n=7
+for i in range(len(A)-1):
+    if A[i]==n:
+        print('the number is there')
+        break
+else:
+    print('the number is not there')
 
-    for i in range(len(A)-1):
-        if A[i]==n:
-            print('the number is there')
-            break
-    else:
-        print('the number is not there')
-search(1)
